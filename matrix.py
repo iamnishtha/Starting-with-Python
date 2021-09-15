8a5dbec251641a3c30b8ea52a1d167fbaf5eb628
Nishtha Srivastava <51073931+srivastavanishtha@users.noreply.github.com>
2021-09-15 16:10:37 +0530

    Update matrix.py

--- matrix.py
+++ matrix.py
@@ -1,1 +1,37 @@
+A = [[1, 4, 5, 12], 
+    [-5, 8, 9, 0],
+    [-6, 7, 11, 19]]
 
+print("A =", A) 
+print("A[1] =", A[1])      # 2nd row
+print("A[1][2] =", A[1][2])   # 3rd element of 2nd row
+print("A[0][-1] =", A[0][-1])   # Last element of 1st Row
+
+column = [];        # empty list
+for row in A:
+  column.append(row[2])   
+
+print("3rd column =", column)
+
+import numpy as np
+
+A = np.array([[1, 2, 3], [3, 4, 5]])
+print(A)
+
+A = np.array([[1.1, 2, 3], [3, 4, 5]]) # Array of floats
+print(A)
+
+A = np.array([[1, 2, 3], [3, 4, 5]], dtype = complex) # Array of complex numbers
+print(A)
+
+zeors_array = np.zeros( (2, 3) )
+print(zeors_array)
+
+'''
+ Output:
+ [[0. 0. 0.]
+  [0. 0. 0.]]
+'''
+
+ones_array = np.ones( (1, 5), dtype=np.int32 ) // specifying dtype
+print(ones_array)      # Output: [[1 1 1 1 1]]
